1bd2db951d74fe1c50e9ee940cf736b891cf5f83
Yeongse <yeongsekm@gmail.com>
2023-11-26 00:23:09 +0900

    :sparkles: 公開用の準備完了

--- lottery/lottery/urls.py
+++ lottery/lottery/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('extract/', include('extract.urls'), name='extract')
+    # path('admin/', admin.site.urls),
+    # path('extract/', include('extract.urls'), name='extract')
+    path('', include('extract.urls'), name='extract')
 ]
